e74eff4264b0bf3ce07ae12c88b58e55964385de
pat <pat@VITUNIVERSITY.LOCAL>
2025-12-18 16:55:24 +0530

    second commit

---
+++ library.py
@@ -0,0 +1,11 @@
+print("this is the first line")
+# Assign the numbers to variables
+num1 = 5
+num2 = 10
+
+# Multiply the numbers and store the result
+product = num1 * num2
+
+# Print the result
+print(f"The product of {num1} and {num2} is: {product}")
+
